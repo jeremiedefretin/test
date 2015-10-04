b1f895d4240d2edaf328bc6b31c7861e35332308
jeremiedefretin1 <jeremiedefretin>
2015-10-04 23:28:42 +0200

    bug

--- Jacquot/src/Jacquot.java
+++ Jacquot/src/Jacquot.java
@@ -1,12 +1,14 @@
 
 public class Jacquot {
+	
+	int a;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("kikoo");
 	}
 	public void neFaitRien(){
-		
+		a =5;
 	}
 
 }
